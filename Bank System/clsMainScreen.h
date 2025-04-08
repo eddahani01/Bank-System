6e02bfc09dfe9df05c466561e7b47df2b3102ad0
eddahani01 <jawadeddahani@outlook.com>
2025-04-08 18:36:44 +0100

    Edit Main Menu  - Added List Clients Screen

--- Bank System/clsMainScreen.h
+++ Bank System/clsMainScreen.h
@@ -2,6 +2,7 @@
 #include <iostream>
 #include "clsScreen.h"
 #include "clsInputValidate.h"
+#include "clsClientListScreen.h"
 #include <iomanip>
 
 
@@ -35,8 +36,8 @@ private:
 
     static void _ShowAllClientsScreen()
     {
-        cout << "\nClient List Screen Will be here...\n";
-
+        //cout << "\nClient List Screen Will be here...\n";
+        clsClientListScreen::ShowClientsList();
 
     }
 
